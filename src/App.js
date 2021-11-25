c9fce9c4b66c489cbb58a79ada8805b000fb796d
DesmondTo <e0550619@u.nus.edu>
2021-11-25 19:20:44 +0800

    Add switch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import NavigationBar from './components/NavigationBar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Schedule from './pages/Schedule';
-import Competition from './pages/Competition';
-import Contacts from './pages/Contacts';
+import NavigationBar from "./components/NavigationBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Schedule from "./pages/Schedule";
+import Competition from "./pages/Competition";
+import Contacts from "./pages/Contacts";
 
 function App() {
   return (
     <div>
       <NavigationBar />
-      <Route path="/Home">
-        <Home />
-      </Route>
-      <Route path="/About">
-        <About />
-      </Route>
-      <Route path="/Schedule">
-        <Schedule />
-      </Route>
-      <Route path="/Competition">
-        <Competition />
-      </Route>
-      <Route path="/Contacts">
-        <Contacts />
-      </Route>
+      <Switch>
+        <Route path="/Home">
+          <Home />
+        </Route>
+        <Route path="/About">
+          <About />
+        </Route>
+        <Route path="/Schedule">
+          <Schedule />
+        </Route>
+        <Route path="/Competition">
+          <Competition />
+        </Route>
+        <Route path="/Contacts">
+          <Contacts />
+        </Route>
+      </Switch>
     </div>
   );
 }
