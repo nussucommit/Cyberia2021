58416b155b8c9ffa624374940d6e1f944d58275a
DesmondTo <e0550619@u.nus.edu>
2021-12-02 00:41:12 +0800

    Implement competition detail page

--- src/pages/Competition.js
+++ src/pages/Competition.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import { useState } from "react";
 
-function Competition() {
-    return (
-        <div>
-            Competition Rule
-        </div>
-    )
+import Fade from "react-reveal";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import Section from "../components/Section";
+import { competitionInfo } from "../data/CompetitionInfo";
+
+/**
+ * Competition detail page of Cyberia 2021 website.
+ * Summarizing info on competition story, details and rules.
+ */
+export default function Competition() {
+  const infoList = competitionInfo;
+
+  const [current, setCurrent] = useState(-1);
+
+  const changeState = (panel) => (e, newValue) => {
+    setCurrent(newValue ? panel : -1);
+  };
+
+  return (
+    <Section
+      info={
+        <Fade left>
+          {infoList.map((info, index) => (
+            <Accordion
+              key={index}
+              expanded={current === index}
+              onChange={changeState(index)}
+            >
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>{info.key}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>{info.value}</AccordionDetails>
+            </Accordion>
+          ))}
+        </Fade>
+      }
+      justifyInfoContent="flex-start"
+      media="Competition Media"
+      displayMedia="none"
+    />
+  );
 }
-
-export default Competition
