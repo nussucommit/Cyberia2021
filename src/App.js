3a332d2c489b6a8e1c45e11a96e742024eb7c17c
DesmondTo <e0550619@u.nus.edu>
2021-12-02 00:39:53 +0800

    Update highest level design

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import styled from "styled-components";
 
-import NavigationBar from "./components/NavigationBar";
-import Home from "./pages/Home";
+import SocialMediaPanel from "./components/SocialMediaPanel";
 import About from "./pages/About";
-import Schedule from "./pages/Schedule";
 import Competition from "./pages/Competition";
-import Contacts from "./pages/Contacts";
+import Contacts from "./pages/Contact";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Schedule from "./pages/Schedule";
 
+/**
+ * React app for Cyberia 2021 organized by NUSSU commIT.
+ *
+ * @version 1.0.0
+ * @author [Desmond To](https://github.com/DesmondTo)
+ */
 function App() {
   return (
-    <div>
-      <NavigationBar />
+    <MajorContainer>
+      <SocialMediaPanel />
       <Routes>
         <Route path="/" element={<Navigate to="/Home" />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Schedule" element={<Schedule />} />
         <Route path="/Competition" element={<Competition />} />
-        <Route path="/Contacts" element={<Contacts />} />
+        <Route path="/Contact" element={<Contacts />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </div>
+    </MajorContainer>
   );
 }
 
 export default App;
+
+const MajorContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-image: url("/assets/images/Darkened Blue Paint.png");
+`;
