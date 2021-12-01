8685eeb89522e9ad45317297cf77c88f84148f16
DesmondTo <e0550619@u.nus.edu>
2021-12-02 00:40:15 +0800

    Implement about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import Section from "../components/Section";
+import AboutImageList from "../data/AboutImageList";
+import { aboutInfoList } from "../data/AboutInfoList";
 
-function About() {
-    return (
-        <div>
-            About Cyberia 2021
-        </div>
-    )
+/**
+ * About page of Cyberia 2021 website.
+ * Summarizing info on Cyberia 2021, Acadia Training and trainer.
+ */
+export default function About() {
+  const infoList = aboutInfoList;
+
+  return (
+    <Section
+      info={
+        infoList.map((info) => info)
+      }
+      justifyInfoContent="flex-start"
+      media={<AboutImageList />}
+    />
+  );
 }
-
-export default About
